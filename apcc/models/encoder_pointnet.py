665a5807734543819b55b744c67a5e210796fc96
Alex Diaz <j.a.diaz@utexas.edu>
2025-12-12 14:29:17 -0600

    [fix] Update encoder

--- apcc/models/encoder_pointnet.py
+++ apcc/models/encoder_pointnet.py
@@ -3,48 +3,42 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 # https://github.com/itberrios/3D/blob/main/point_net/point_net.py
+# PCN: Point Completion Network
 
 class PointNet(nn.Module):
-    def __init__(self, num_global_feats=1024):
-        '''Parameters:
-            num_points: number of input points in point cloud
-            num_global_feats: dimension of global feature vector
-            local_feat: whether to concatenate local and global features
-        '''
-        super(PointNet, self).__init__()
+    def __init__(self, latent_dim=1024):
+        super().__init__()
+        self.latent_dim = latent_dim
 
-        self.num_global_feats = num_global_feats
+        self.first_conv = nn.Sequential(
+            nn.Conv1d(3, 128, 1),
+            nn.BatchNorm1d(128),
+            nn.ReLU(inplace=True),
+            nn.Conv1d(128, 256, 1),
+            nn.BatchNorm1d(256),
+            nn.ReLU(inplace=True),
+        )
 
-        # Shared MLP 1
-        self.conv1 = nn.Conv1d(3, 64, kernel_size=1)
-        self.conv2 = nn.Conv1d(64, 64, kernel_size=1)
+        self.second_conv = nn.Sequential(
+            nn.Conv1d(512, 512, 1),
+            nn.BatchNorm1d(512),
+            nn.ReLU(inplace=True),
+            nn.Conv1d(512, latent_dim, 1),
+            nn.BatchNorm1d(latent_dim),
+            nn.ReLU(inplace=True),
+        )
 
-        # Shared MLP 2
-        self.conv3 = nn.Conv1d(64, 64, kernel_size=1)
-        self.conv4 = nn.Conv1d(64, 128, kernel_size=1)
-        self.conv5 = nn.Conv1d(128, self.num_global_feats, kernel_size=1)
+    def forward(self, x, return_g=False):
+        # x: [B, N, 3] -> [B, 3, N]
+        x = x.transpose(1, 2)
 
-        # Batch norms
-        self.bn1 = nn.BatchNorm1d(64)
-        self.bn2 = nn.BatchNorm1d(64)
-        self.bn3 = nn.BatchNorm1d(64)
-        self.bn4 = nn.BatchNorm1d(128)
-        self.bn5 = nn.BatchNorm1d(self.num_global_feats)
+        feat = self.first_conv(x)                 # [B, 256, N]
+        g = torch.max(feat, dim=2).values         # [B, 256]
 
-    def forward(self, x):
-        """
-        x: [B, N, 3] 
-        """
-        x = x.permute(0, 2, 1) # [B, 3, N]
+        g_exp = g.unsqueeze(-1).expand(-1, -1, feat.size(2))   # [B, 256, N]
+        feat2 = torch.cat([feat, g_exp], dim=1)                # [B, 512, N]
 
-        x = self.bn1(F.relu(self.conv1(x)))   # [B, 64, N]
-        x = self.bn2(F.relu(self.conv2(x)))   # [B, 64, N]
+        feat2 = self.second_conv(feat2)          # [B, latent_dim, N]
+        v = torch.max(feat2, dim=2).values       # [B, latent_dim]  (1024)
 
-        # Second shared MLP
-        x = self.bn3(F.relu(self.conv3(x)))   # [B, 64, N]
-        x = self.bn4(F.relu(self.conv4(x)))   # [B, 128, N]
-        x = self.bn5(F.relu(self.conv5(x)))   # [B, 1024, N] 
-
-        global_features = torch.max(x, dim=2)[0]
- 
-        return global_features
+        return (v, g) if return_g else v
